e8e845ff9518a289f47bc113a9f457458b93fba2
Jeremy Pedersen <jeremydavispedersen@gmail.com>
2019-10-11 19:15:53 +0800

    Cleaned up index.py, still need to add code to call certbot and make changes to OSS

--- get-ssl-certs/index.py
+++ get-ssl-certs/index.py
@@ -1,39 +1,27 @@
 import logging
-import requests
-import json
-from bs4 import BeautifulSoup as bs
 
-# A simple Function Compute example that will produce
-# the page title for any webpage (URL) it is given as input
-# 
-# This function relies on the BeautifulSoup and requests
-# libraries, and is a good way to test your understanding of
-# the 'fun' command, which is needed to pull in non-standard
-# dependencies like BeautifulSoup
+# A simple Function Compute function to renew Let's Encrypt
+# certificates using certbot, and bind them to an OSS bucket
 def handler(event, context):
+
   # Get logging info
   logger = logging.getLogger()
 
   # Log entire raw event in case we need to do troubleshooting later
   logger.info(event)
 
-  # Load URL (from event string)
+  # Load event string
   try:
-    url = str(event.decode()).strip()
-  except:
-    pageTitle = "ERROR: Failed to decode event string passed to FC...is it a valid URL?"
+    params_raw = str(event.decode()).strip()
+  except DecodeEventString:
+    response = "ERROR: Failed to decode event string passed to FC"
 
-  # Fetch page with requests, and convert to a BeautifulSoup object
+  # Parse event JSON
   try:
-    r = requests.get(url)
-    page = bs(r.text)
-  except:
-    pageTitle = "ERROR: Failed to fetch page, check URL and ensure it points at a valid HTML document"
+    params_json = json.loads(params_raw)
+  except DecodeEventJSON:
+    response = "ERROR: Unable to convert event string to valid JSON"
 
-  # Extract page title
-  try:
-    pageTitle = page.title.text
-  except:
-    pageTitle = "ERROR: This page has no <title> tag"
-
-  return pageTitle
+  # TODO: Add code to perform challenge (by placing file on OSS), get certs, and subsequently install them
+  
+  return response
